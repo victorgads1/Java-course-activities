779f9a85d4caef9194994f44d7387c6617367afe
victorgads1 <victorgads123@gmail.com>
2025-12-11 22:21:05 -0300

    atualização atividade

--- atividade_1/src/primeiro_print.java
+++ atividade_1/src/primeiro_print.java
@@ -4,13 +4,14 @@ public class primeiro_print {
 	public static void main(String[] args) {
 		
 		int a = 45;
+		int d = 12;
 		double x = 12.312;
 		
 	System.out.println(a);
 	System.out.println(x);
 	System.out.printf("%.3f%n",x);
 	System.out.println("eu preciso de " + a + " reais ");
-	System.out.printf("o cachorro andou metros", + x  + "e descansou minutos" );
+	System.out.printf("o cachorro andou " + a +" minutos e descansou " + d + " minutos");
 
 	}
 
