7f028b3ce1ac96c1b934ee78d026d3e7a56fa610
victorgads1 <victorgads123@gmail.com>
2025-12-16 18:09:39 -0300

    processamento de dados 1

---
+++ processamento_de_dados/src/p_dados.java
@@ -0,0 +1,25 @@
+public class p_dados {
+	
+
+	public static void main(String[] args) {
+		
+	int a,h,b,c,d ;
+	
+	a = 6;
+	h = 8;
+	b = 8;
+	c = 12;
+	d = 11;
+	int dois = 2;
+	
+	int area = a + b + c + d ;
+	double A = h/(b+d); 
+	
+	
+	System.out.println(area);
+	System.out.println(a);
+	
+
+	}
+
+}
